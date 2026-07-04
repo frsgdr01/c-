5c12c7a9769de43d4db0be5a3b82265159c40880
Izmailov <frsgdr01@gmail.com>
2026-07-04 12:59:19 +0200

    8

--- main.cpp
+++ main.cpp
@@ -1,75 +1,60 @@
 #include <iostream>
-#include <windows.h>
+#include <cstring>
+#include <cctype>
 
-int calculate(char op, int first, int second) {
-    if (op == '/') {
-        if (!second) {
-            std::cout << "You cannot divide by 0\n";
-            return 0;
+using namespace std;
+
+bool isPalindrome(char str[]);
+
+int main()
+{
+    char str[1000] = "\0";
+
+    cout << "Enter text: ";
+    cin.getline(str, 1000);
+
+    if (isPalindrome(str))
+    {
+        cout << "Palindrome";
+    }
+    else
+    {
+        cout << "Not palindrome";
+    }
+
+    return 0;
+}
+
+bool isPalindrome(char str[])
+{
+    int len = strlen(str);
+    int left = 0;
+    int right = len - 1;
+
+    while (left < right)
+    {
+        if (str[left] == ' ')
+        {
+            ++left;
+            continue;
         }
-        return first / second;
-    }
-    if (op == '+') return first + second;
-    if (op == '-') return first - second;
-    if (op == '*') return first * second;
-    return 0;
-}
-
-float calculate(char op, float first, float second) {
-    if (op == '/') {
-        if (second == 0.0f) {
-            std::cout << "You cannot divide by 0\n";
-            return 0.0f;
+        if (str[right] == ' ')
+        {
+            --right;
+            continue;
         }
-        return first / second;
+
+        char leftChar = tolower(str[left]);
+        char rightChar = tolower(str[right]);
+
+        if (leftChar != rightChar)
+        {
+            return false;
+        }
+
+        ++left;
+        --right;
     }
-    if (op == '+') return first + second;
-    if (op == '-') return first - second;
-    if (op == '*') return first * second;
-    return 0.0f;
-}
 
-int calculate(const char* op, const int* first, const int* second) {
-    if (op && first && second) {
-        return calculate(*op, *first, *second);
-    }
-    return 0;
-}
-
-int& processFirstBy(int& first, const int by, const char op) {
-    first = calculate(op, first, by);
-    return first;
-}
-
-int main() {
-    SetConsoleCP(65001);
-    SetConsoleOutputCP(65001);
-
-    int* firstPtr = new int;
-    int* secondPtr = new int;
-    char* opPtr = new char;
-
-    std::cout << "Enter first number: ";
-    std::cin >> *firstPtr;
-
-    std::cout << "Enter op: ";
-    std::cin >> *opPtr;
-
-    std::cout << "Enter second number: ";
-    std::cin >> *secondPtr;
-
-    int result = calculate(opPtr, firstPtr, secondPtr);
-    std::cout << "Result: " << result << "\n";
-
-    delete firstPtr;
-    delete secondPtr;
-    delete opPtr;
-    firstPtr = nullptr;
-    secondPtr = nullptr;
-    opPtr = nullptr;
-
-    int f = 10;
-    processFirstBy(f, 5, '+');
-
-    return 0;
+    return true;
 }
