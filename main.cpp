4122a68cabc9294e2053501c0d573868130024f5
Izmailov <frsgdr01@gmail.com>
2026-07-04 23:35:37 +0200

    9

--- main.cpp
+++ main.cpp
@@ -1,60 +1,17 @@
 #include <iostream>
-#include <cstring>
-#include <cctype>
+#include "flip.h"
 
 using namespace std;
 
-bool isPalindrome(char str[]);
-
 int main()
 {
-    char str[1000] = "\0";
+    char str[] = "123456";
 
-    cout << "Enter text: ";
-    cin.getline(str, 1000);
+    cout << "Original: " << str << endl;
 
-    if (isPalindrome(str))
-    {
-        cout << "Palindrome";
-    }
-    else
-    {
-        cout << "Not palindrome";
-    }
+    flipArray(str);
+
+    cout << "Flipped: " << str << endl;
 
     return 0;
-}
-
-bool isPalindrome(char str[])
-{
-    int len = strlen(str);
-    int left = 0;
-    int right = len - 1;
-
-    while (left < right)
-    {
-        if (str[left] == ' ')
-        {
-            ++left;
-            continue;
-        }
-        if (str[right] == ' ')
-        {
-            --right;
-            continue;
-        }
-
-        char leftChar = tolower(str[left]);
-        char rightChar = tolower(str[right]);
-
-        if (leftChar != rightChar)
-        {
-            return false;
-        }
-
-        ++left;
-        --right;
-    }
-
-    return true;
 }
