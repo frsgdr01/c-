4821219873a487c579eb6c6ca5c3abbfd4f81043
Izmailov <frsgdr01@gmail.com>
2026-07-06 13:12:42 +0200

    11

--- main.cpp
+++ main.cpp
@@ -2,46 +2,78 @@
 
 using namespace std;
 
-int memory = 0;
-
-void setStartValue(int value)
+class Calculator
 {
-    memory = value;
-}
-
-int calculate(char operation, int value)
-{
-    switch (operation)
+public:
+    void setStartValue(int start)
     {
+        memory = start;
+    }
+
+    int calculate(int right, char op)
+    {
+        switch (op)
+        {
         case '+':
-            memory += value;
+            memory += right;
             break;
         case '-':
-            memory -= value;
+            memory -= right;
             break;
         case '*':
-            memory *= value;
+            memory *= right;
             break;
         case '/':
-            if (value != 0)
-            {
-                memory /= value;
-            }
+            if (right != 0)
+                memory /= right;
             break;
-        default:
-            break;
+        }
+        return memory;
     }
-    return memory;
-}
+
+    int calculate(int left, int right, char op)
+    {
+        switch (op)
+        {
+        case '+':
+            memory = left + right;
+            break;
+        case '-':
+            memory = left - right;
+            break;
+        case '*':
+            memory = left * right;
+            break;
+        case '/':
+            if (right != 0)
+                memory = left / right;
+            else
+                memory = 0;
+            break;
+        }
+        return memory;
+    }
+
+private:
+    int memory = 0;
+};
 
 int main()
 {
-    setStartValue(10);
+    Calculator calc1;
 
-    cout << calculate('+', 5) << endl;
-    cout << calculate('*', 2) << endl;
-    cout << calculate('-', 8) << endl;
-    cout << calculate('/', 2) << endl;
+    calc1.setStartValue(10);
+
+    cout << calc1.calculate(5, '+') << endl;
+    cout << calc1.calculate(2, '*') << endl;
+    cout << calc1.calculate(8, '-') << endl;
+    cout << calc1.calculate(2, '/') << endl;
+
+    cout << "---" << endl;
+
+    Calculator calc2;
+
+    cout << calc2.calculate(50, 5, '/') << endl;
 
     return 0;
 }
