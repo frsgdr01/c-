82cbc8e2aeb609fa302ba68f74439b038ab8b9b8
Izmailov <frsgdr01@gmail.com>
2026-07-07 21:09:15 +0200

    14

--- main.cpp
+++ main.cpp
@@ -1,41 +1,80 @@
 #include <iostream>
-#include <cstring>
 
-using namespace std;
+class vector {
+private:
+    unsigned int size = 0;
+    int* dinamycArray = nullptr;
 
-bool isPalindrome(const char* str) {
-    int start = 0;
-    int end = strlen(str) - 1;
-
-    while (start < end) {
-        if (str[start] != str[end]) {
-            return false;
-        }
-        start++;
-        end--;
+public:
+    vector(int sizeOffArray) {
+        size = sizeOffArray;
+        dinamycArray = new int[size]{0};
     }
-    return true;
-}
+
+    ~vector() {
+        delete[] dinamycArray;
+    }
+
+    void resize(int newSize) {
+        if (newSize < 0) return;
+
+        int* newArray = new int[newSize]{0};
+        int elementsToCopy = (newSize < size) ? newSize : size;
+
+        for (int i = 0; i < elementsToCopy; ++i) {
+            newArray[i] = dinamycArray[i];
+        }
+
+        delete[] dinamycArray;
+        dinamycArray = newArray;
+        size = newSize;
+    }
+
+    void set(int index, int newValue) {
+        if (index >= 0 && index < static_cast<int>(size)) {
+            dinamycArray[index] = newValue;
+        }
+    }
+
+    int get(int index) const {
+        if (index >= 0 && index < static_cast<int>(size)) {
+            return dinamycArray[index];
+        }
+        return 0;
+    }
+
+    unsigned int getSize() const {
+        return size;
+    }
+};
 
 int main() {
-    int size = 0;
-    cout << "Enter max text size: ";
-    cin >> size;
+    vector myVector(5);
 
-    cin.ignore();
-
-    char* str = new char[size + 1];
-
-    cout << "Enter text: ";
-    cin.getline(str, size + 1);
-
-    if (isPalindrome(str)) {
-        cout << "Palindrome" << endl;
-    } else {
-        cout << "Not palindrome" << endl;
+    for (int i = 0; i < 5; ++i) {
+        myVector.set(i, (i + 1) * 10);
     }
 
-    delete[] str;
+    for (int i = 0; i < 5; ++i) {
+        std::cout << myVector.get(i) << " ";
+    }
+    std::cout << "\n";
+
+    myVector.resize(8);
+    myVector.set(5, 60);
+    myVector.set(6, 70);
+    myVector.set(7, 80);
+
+    for (int i = 0; i < 8; ++i) {
+        std::cout << myVector.get(i) << " ";
+    }
+    std::cout << "\n";
+
+    myVector.resize(3);
+    for (int i = 0; i < 3; ++i) {
+        std::cout << myVector.get(i) << " ";
+    }
+    std::cout << "\n";
 
     return 0;
 }
