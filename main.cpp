2db80e7fd3ecbdf7fc9307483c8d73882fc3b2cf
Izmailov <frsgdr01@gmail.com>
2026-07-07 22:19:23 +0200

    16

--- main.cpp
+++ main.cpp
@@ -1,82 +1,98 @@
 #include <iostream>
+#include <string>
+#include <windows.h>
 
-using namespace std;
-
-class Calculator
-{
-public:
-    Calculator()
-    {
-        memory = 0;
-    }
-
-    Calculator(int startValue)
-    {
-        memory = startValue;
-    }
-
-    void setStartValue(int start)
-    {
-        memory = start;
-    }
-
-    int calculate(char op, int right)
-    {
-        switch (op)
-        {
-        case '+':
-            memory += right;
-            break;
-        case '-':
-            memory -= right;
-            break;
-        case '*':
-            memory *= right;
-            break;
-        case '/':
-            if (right != 0)
-                memory /= right;
-            break;
-        }
-        return memory;
-    }
-
-    int calculate(int left, char op, int right)
-    {
-        switch (op)
-        {
-        case '+':
-            memory = left + right;
-            break;
-        case '-':
-            memory = left - right;
-            break;
-        case '*':
-            memory = left * right;
-            break;
-        case '/':
-            if (right != 0)
-                memory = left / right;
-            else
-                memory = 0;
-            break;
-        }
-        return memory;
-    }
-
+class ColorManager {
 private:
-    int memory;
+    int favoriteColorsMask = 0;
+    const std::string colorNames[13] = {
+        "червоний", "синій", "зелений", "жовтий", "фіолетовий", 
+        "блакитний", "білий", "чорний", "помаранчевий", "бежевий", 
+        "рожевий", "салатовий", "сірий"
+    };
+
+public:
+    void printColorList() {
+        std::cout << "\n";
+        for (int i = 0; i < 13; ++i) {
+            std::cout << i << ". " << colorNames[i] << "\n";
+        }
+    }
+
+    void addColor(int index) {
+        if (index >= 0 && index <= 12) {
+            int i = 1;
+            i = i << index;
+            favoriteColorsMask = favoriteColorsMask | i;
+            std::cout << "додано\n";
+        } else {
+            std::cout << "невірний індекс\n";
+        }
+    }
+
+    void removeColor(int index) {
+        if (index < 0 || index > 12) {
+            std::cout << "невірний індекс\n";
+            return;
+        }
+        int i = 1;
+        i = i << index;
+        i = ~i;
+        favoriteColorsMask = favoriteColorsMask & i;
+        std::cout << "видалено\n";
+    }
+
+    void showFavorites() {
+        bool hasFavorites = false;
+        for (int index = 0; index < 13; ++index) {
+            int i = 1;
+            i = i << index;
+            if ((favoriteColorsMask & i) > 0) {
+                std::cout << "- " << colorNames[index] << "\n";
+                hasFavorites = true;
+            }
+        }
+        if (!hasFavorites) {
+            std::cout << "порожньо\n";
+        }
+    }
 };
 
-int main()
-{
-    Calculator calc1 = 10;
-    cout << calc1.calculate('-', 10) << endl;
+int main() {
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
 
-    cout << "---" << endl;
+    ColorManager manager;
 
-    Calculator calc2;
-    cout << calc2.calculate(50, '/', 5) << endl;
+    while (true) {
+        std::cout << "\n1. add favorite colour\n2. remove favorite\n3. show favorites\n4. exit\n-: ";
+        int input = 0;
+        std::cin >> input;
 
+        if (4 == input) {
+            return 0;
+        }
+
+        if (1 == input) {
+            manager.printColorList();
+            std::cout << "-: ";
+            int index = 0;
+            std::cin >> index;
+            manager.addColor(index);
+        }
+        else if (2 == input) {
+            manager.printColorList();
+            std::cout << "-: ";
+            int index = 0;
+            std::cin >> index;
+            manager.removeColor(index);
+        }
+        else if (3 == input) {
+            manager.showFavorites();
+        }
+        else {
+            std::cout << "некоректний ввід спробуйте ще раз\n";
+        }
+    }
     return 0;
 }
